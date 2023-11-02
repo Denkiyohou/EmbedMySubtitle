08d3833df68dd4dcf304e359efdb10b23792d526
Denkiyohou <denkiyohou@outlook.com>
2023-11-02 16:26:30 +0800

    Update viewmodel

--- EmbedMySubtitle/ViewModels/MainWindowViewModel.cs
+++ EmbedMySubtitle/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,42 @@
-﻿namespace EmbedMySubtitle.ViewModels
+﻿using ReactiveUI;
+using System;
+using System.IO;
+using FFmpeg;
+using System.Windows.Input;
+using Avalonia.Platform.Storage;
+using Avalonia.Controls;
+
+namespace EmbedMySubtitle.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private string? _videoFilePath;
+        public string? VideoFilePath
+        {
+            get => _videoFilePath;
+            set => this.RaiseAndSetIfChanged(ref _videoFilePath, value);
+        }
+
+        private string? _subtitleFilePath;
+        public string? SubtitleFilePath
+        {
+            get => _subtitleFilePath;
+            set => this.RaiseAndSetIfChanged(ref _subtitleFilePath, value);
+        }
+
+        private string? _outputFilePaht;
+        public string? OutputFilePaht
+        {
+            get => _outputFilePaht;
+            set => this.RaiseAndSetIfChanged(ref _outputFilePaht, value);
+        }
+
+        public ICommand SelectVideoCommand { get; }
+        public ICommand SelectSubtitleCommand { get; }
+        public ICommand SelectOutputPathCommand { get; }
+        public ICommand EmbedSubtitleDCommand { get; }
+
+        
+
     }
 }
